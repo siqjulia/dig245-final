3a8f331f3e7ee131a44a9a03209bb3c77ab08a73
siqjulia <jusiqueira@davidson.edu>
2024-12-02 17:39:59 -0500

    Local storage ability added, reset local storage when "play again" is clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 
 /* javascript */
-let div = document.querySelector(".output");
+let div = document.querySelector(".score");
 console.clear();
 
 // Timer
@@ -32,55 +32,66 @@ function restartTimer() {
 }
 
 // Score keeping
-let game1 = 0;
-let game2 = 0;
-let game3 = 0;
+let game1 = localStorage.getItem("game1") || 0;
+let game2 = localStorage.getItem("game2") || 0;
+let game3 = localStorage.getItem("game3") || 0;
 
+var firstButton = document.querySelector(".btn1");
+var secondButton = document.querySelector(".btn2");
+var thirdButton = document.querySelector(".btn3");
 
-
-var firstButton = document.querySelector("btn");
-var secondButton = document.querySelector("btn2");
-var thirdButton = document.querySelector("#btn3");
 
 function updateTotalScore() {
-    return game1 + game2 + game3;
+        return Number(game1) + Number(game2) + Number(game3);
 }
+
 
 function trackScore() {
-    firstButton.addEventListener("click", () => {
-        if (timeRemaining > 1) {
-            restartTimer();
-        } else {
-            // alert("Time is up! Click the next button to go to the next game.");
-        }
-    });
+    if (game == 1) {
+        firstButton.addEventListener("click", () => {
+            if (timeRemaining > 1) {
+                game1 = 5;
+                localStorage.setItem("game1", game1)
+                restartTimer();
+            } else {
+                // alert("Time is up! Click the next button to go to the next game.");
+            }
+        });
+    }
+    else if (game == 2) {
 
-    secondButton.addEventListener("click", () => {
-        if (timeRemaining > 1) {
-            game2 = 5; //  Score 5 for second button
-            restartTimer();
-        } else {
-            // alert("Time is up! Click the next button to go to the next game.");
-        }
-    });
 
-    thirdButton.addEventListener("click", () => {
-        if (timeRemaining > 1) {
-            game3 = 5; // Score 5 for third button
-            restartTimer();
-            winOrLose();
-        } else if (timeRemaining < 1) {
-            // alert("Time is up! Click next to see the final screen.");
-            winOrLose();
-        }
-    });
+        secondButton.addEventListener("click", () => {
+            if (timeRemaining > 1) {
+                game2 = 5; //  Score 5 for second button
+                localStorage.setItem("game2", game2)
+                restartTimer();
+            } else {
+                // alert("Time is up! Click the next button to go to the next game.");
+            }
+        });
+    }
+    else if (game == 3) {
+        thirdButton.addEventListener("click", () => {
+            if (timeRemaining > 1) {
+                game3 = 5; // Score 5 for third button
+                localStorage.setItem("game3", game3)
+                restartTimer();
+                winOrLose();
+            } else if (timeRemaining < 1) {
+                // alert("Time is up! Click next to see the final screen.");
+                winOrLose();
+            }
+        });
+    }
 }
+
+
 
 function updateDisplay() {
     div.innerHTML = `
-    <h1 class="timeRemaining">Time Remaining: ${timeRemaining}s</h1>
-    <p class="score">Score: ${updateTotalScore()}</p>
-    console.log(timeRemaining);
+    </span class="timeRemaining">Time Remaining: ${timeRemaining}s</span>
+    <span class="score">Score: ${updateTotalScore()}</span>
   `;
 }
 
